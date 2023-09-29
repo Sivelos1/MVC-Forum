7d9135f629ed8b167bf54f1907a41563db84b507
Devin Amlen <devinamlen@gmail.com>
2023-09-29 10:53:52 -0400

    adding signup functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,6 +28,7 @@ router.get('/login', (req, res) => {
   }
 
   res.render('login');
+  res.render('signup');
 });
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,6 @@
 const loginFormHandler = async (event) => {
-  // TODO: Add a comment describing the functionality of this statement
   event.preventDefault();
 
-  // TODO: Add a comment describing the functionality of these expressions
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
