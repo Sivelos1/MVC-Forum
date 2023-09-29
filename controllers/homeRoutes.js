e9b30b2673700014089e4a3bcb1eeadbbcedc5cf
Devin Amlen <devinamlen@gmail.com>
2023-09-29 11:59:31 -0400

    changing homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,21 +1,28 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
-  console.log("is this the error teehee");
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
+    const postData = await Post.findAll({
+      order:[['time','ASC']]
+    })
+    if(postData){
+      const posts = postData.map((post) => post.get({plain: true}));
+      res.render('homepage', {
+        posts,
+        hasPosts: true,
+        logged_in: req.session.logged_in,
+      });
+    }
+    else{
+      res.render('homepage', {
+        hasPosts: false,
+        logged_in: req.session.logged_in,
+      });
+    }
 
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      logged_in: req.session.logged_in,
-    });
+    
   } catch (err) {
     res.status(500).json(err);
   }
