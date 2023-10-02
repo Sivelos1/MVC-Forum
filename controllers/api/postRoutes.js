3bc38f21f4ae1cbeaaa9c120defbd82d7bb0d67a
Devin Amlen <devinamlen@gmail.com>
2023-10-02 17:50:57 -0400

    Update postRoutes.js

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
 
-router.post('/newPost', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const newPost = await Post.create({
       title: req.body.title,
@@ -9,12 +9,11 @@ router.post('/newPost', async (req, res) => {
       author: req.session.user_id,
       time: Date.now(),
     });
+    console.log(newPost);
     res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
   }
 });
-
-
 
 module.exports = router;
