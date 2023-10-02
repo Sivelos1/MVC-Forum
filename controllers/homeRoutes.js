509bc39fcef479506914c96d0427ac12b253cf03
Devin Amlen <devinamlen@gmail.com>
2023-10-02 12:02:08 -0400

    adding ability to make posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,6 +28,16 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
+router.get('/newPost', withAuth, async (req, res) => {
+  try {
+    res.render('newPost', {
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/');
