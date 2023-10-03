2f13b18bf963d0e05b6d652a8511ac5ecce44fdd
Devin Amlen <devinamlen@gmail.com>
2023-10-03 12:13:54 -0400

    help me

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,7 +1,8 @@
-const newPostButton = document.querySelector('#makeNewPost');
+const newPostButton = document.querySelector('.makeNewPost');
 
 const makeNewPostHandler = async (event) => {
   event.preventDefault();
+  console.log("please fucking do something ffs");
   const response = await fetch('/newPost', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
