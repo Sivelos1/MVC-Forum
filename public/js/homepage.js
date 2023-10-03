7992682efafd8b0779e6eba4ac236ee1afb24493
Devin Amlen <devinamlen@gmail.com>
2023-10-03 12:25:55 -0400

    Update homepage.js

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -2,17 +2,8 @@ const newPostButton = document.querySelector('.makeNewPost');
 
 const makeNewPostHandler = async (event) => {
   event.preventDefault();
-  console.log("please fucking do something ffs");
-  const response = await fetch('/newPost', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-  });
-
-  if (response.ok) {
-    document.location.replace('/newPost');
-  } else {
-    alert('Failed to reach new page');
-  }
+  
+  document.location.replace('/newPost');
 };
 
 newPostButton.addEventListener('submit', makeNewPostHandler);
